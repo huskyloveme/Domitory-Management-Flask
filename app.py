436d68348ce0c7114fd76f8648c684921956c8fd
Mark <trumvang1997@gmail.com>
2023-10-09 04:20:13 +0700

    update

--- app.py
+++ app.py
@@ -1,41 +1,42 @@
 from flask import Flask, render_template, request
 from flask_paginate import Pagination, get_page_parameter
-# from databases.connect_mysql import cursor, connection
+from databases.connect_mysql import cursor, connection
+import math
 
 
-
-
-app = Flask(__name__, template_folder='templates')
-
-
-# @app.route('/')
-# def home():
-#     page = request.args.get('page', 1, type=int)
-#     limit = 15
-#
-#     # Take the column name
-#     query = "show columns FROM final_data"
-#     cursor.execute(query)
-#     result_col = cursor.fetchall()
-#     col = [i[0] for i in result_col]
-#
-#     # Take the data
-#     query = "select * from final_data limit " + str(limit) + " offset " + str(int(page)*limit)
-#     cursor.execute(query)
-#     result_data_col = cursor.fetchall()
-#     data_col = [i for i in result_data_col]
-#
-#     #Count page_data
-#     query = "select count(1) from final_data"
-#     cursor.execute(query)
-#     result_data_col = cursor.fetchall()
-#     count_data = int(result_data_col[0][0])
-#
-#     return render_template("home/home.html", col=col, data_col=data_col, page = int(page), count_data=count_data, total_page=round(count_data/limit))
+app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def home():
-    return "Hello"
+    return render_template("index.html")
+
+@app.route('/show_database_<table_name>')
+def show_database(table_name):
+    page = request.args.get('page', 1, type=int)
+    table_name = table_name
+    limit = 15
+    # Take the column name
+    query = f"show columns FROM {table_name}"
+    cursor.execute(query)
+    result_col = cursor.fetchall()
+    col = [i[0] for i in result_col]
+
+    # Take the data
+    query = f"select * from {table_name} limit " + str(limit) + " offset " + str(int(page-1)*limit)
+    cursor.execute(query)
+    result_data_col = cursor.fetchall()
+    data_col = [i for i in result_data_col]
+
+    #Count page_data
+    query = f"select count(1) from {table_name}"
+    cursor.execute(query)
+    result_data_col = cursor.fetchall()
+    count_data = int(result_data_col[0][0])
+
+    return render_template("home/home.html", col=col, data_col=data_col, page = int(page), count_data=count_data, total_page=math.ceil(count_data/limit))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- databases/connect_mysql.py
+++ databases/connect_mysql.py
@@ -1,14 +1,15 @@
 import mysql.connector
 
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'data_craw'
+    'host': '34.143.147.167',
+    'user': 'admin',
+    'password': 'Admin123!@#',
+    'database': 'domitory_management',
+    'port': '3306'
 }
 connection = mysql.connector.connect(**db_config)
 cursor = connection.cursor()
-query = "select count(1) from final_data"
-cursor.execute(query)
-result_col = cursor.fetchall()
-print(result_col)
+# query = "select count(1) from students"
+# cursor.execute(query)
+# result_col = cursor.fetchall()
+# print(result_col)
